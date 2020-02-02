00529fa69ac00b92724caf15c03a3e701e9b8871
Kevin Callanan <56414722+kevincallanan@users.noreply.github.com>
2020-02-02 15:36:35 -0600

    Changes to be committed: 	new file:   Disable-CAPI2Logging.ps1 	new file:   Enable-CAPI2Logging.ps1

---
+++ Disable-CAPI2Logging.ps1
@@ -0,0 +1,28 @@
+﻿# ==============================================================================================
+#    NAME: CAPI2Logging.ps1
+#    AUTHOR: Kevin Callanan
+#    Released Version: 1.0
+#	 Description: This script will connect to each server provided and disable the CAPI2/Operational
+#	 Windows Event Log located under /Applications and Sevices Logs/Microsoft/CAPI2/Operational	
+#    WARRENTY: None - Proceed at your own risk :-) 
+# ==============================================================================================
+
+$Servers = @(Import-CSV -Path '.\servers.txt')
+
+If ($Servers.count -like 0){
+Write-Host 'No Servers Listed' 
+Exit
+}
+Else{
+Write-Host "Total Server Count: " -NoNewline
+Write-Host $Servers.count
+}
+
+ForEach ($i in $Servers) {
+
+    $Server = $i
+    Write-Host 'Working on Server' $Server -BackgroundColor Yellow -ForegroundColor Black
+    Invoke-Command -ComputerName $Server -ScriptBlock { 
+	wevtutil.exe sl Microsoft-Windows-CAPI2/Operational /e:false
+    }
+ }
