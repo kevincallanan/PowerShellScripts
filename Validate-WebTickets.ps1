138a36843da78734cdf0e46a6fde096fb32376d8
Kevin Callanan <56414722+kevincallanan@users.noreply.github.com>
2020-02-11 20:21:59 -0600

    New Script Add 2/11/20

---
+++ Validate-WebTickets.ps1
@@ -0,0 +1,12 @@
+﻿$Pools = Get-CsPool |WHERE {$_.Identity -like 'skfe*'} |SELECT Identity
+
+ForEach ($Pool in $Pools) {
+    $Servers = Get-CSPool -Identity $Pool.Identity |SELECT -ExpandProperty Computers
+
+    ForEach ($Server in $Servers){
+    $WebCall = 'https://'+ $Server + '/WebTicket/WebTicketService.svc/mex'
+    #Write-Host $Webcall
+    $StatusDetails = Invoke-WebRequest -Uri $WebCall |SELECT StatusCode,StatusDescription,Headers
+    $StatusDetails
+    }
+}
