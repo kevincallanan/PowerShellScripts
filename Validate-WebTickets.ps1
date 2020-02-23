e2a0508dc335c200cb8cb0d923ccf44e071b2d7f
Kevin Callanan <56414722+kevincallanan@users.noreply.github.com>
2020-02-22 23:50:25 -0600

    updated script registrar search and screen output 2/22/20

--- Validate-WebTickets.ps1
+++ Validate-WebTickets.ps1
@@ -1,8 +1,8 @@
-﻿$Pools = Get-CsPool |WHERE {$_.Identity -like 'skfe*'} |SELECT Identity
+﻿$Pools = Get-CsPool | WHERE {$_.Services -like "Registrar:*"}|Sort-Object Identity | SELECT Identity
 
 ForEach ($Pool in $Pools) {
     $Servers = Get-CSPool -Identity $Pool.Identity |SELECT -ExpandProperty Computers
-
+    Write-Host $Pool.Identity -ForegroundColor Yellow -BackgroundColor Black
     ForEach ($Server in $Servers){
     $WebCall = 'https://'+ $Server + '/WebTicket/WebTicketService.svc/mex'
     #Write-Host $Webcall
